988a64a68336d18ad542a28efcf6d5226534103f
MirandaNichols123 <143537615+MirandaNichols123@users.noreply.github.com>
2024-02-20 11:46:47 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+
+
+class Hello
+{
+    static void Main()
+    {
+        // This line prints "Hello, World" 
+        Console.WriteLine("Hello, World");
+    }
+}
